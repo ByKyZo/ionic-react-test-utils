46343df1873bc77185f3a638021aca942d3c28b3
ByKyZo <alexis.robert.dev@gmail.com>
2022-06-23 16:15:13 +0200

    fix/checkbox-checked-not-updated

--- src/mocks/mockIonCheckbox.tsx
+++ src/mocks/mockIonCheckbox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 export const mockIonCheckbox: React.FC<{
   checked: boolean,
@@ -6,11 +6,21 @@ export const mockIonCheckbox: React.FC<{
   onIonFocus: (e: CustomEvent) => void,
   onIonBlur: (e: CustomEvent) => void,
   onClick: (e: React.SyntheticEvent<MouseEvent>) => void;
-}> = ({ checked, onIonChange, onIonFocus, onIonBlur, onClick, ...rest }) => {
+}> = ({ onIonChange, onIonFocus, onIonBlur, onClick, ...rest }) => {
 
   return (
-    <input type="checkbox" onClick={e => {
-      onIonChange(new CustomEvent('ionChange', { detail: { checked: e.currentTarget.checked } }));
-    }} onFocus={() => { onIonFocus(new CustomEvent('ionFocus'))}} onBlur={() => { onIonBlur(new CustomEvent('ionBlur'))}} role="toggle" {...rest} />
+    <input
+        type="checkbox"
+        onClick={e => {
+          onIonChange(new CustomEvent('ionChange', { detail: { checked: e.currentTarget.checked } }));
+        }}
+        onFocus={() => {
+          onIonFocus(new CustomEvent('ionFocus'))
+        }}
+        onBlur={() => {
+          onIonBlur(new CustomEvent('ionBlur'))
+        }}
+        role="toggle" {...rest}
+    />
   );
 };
